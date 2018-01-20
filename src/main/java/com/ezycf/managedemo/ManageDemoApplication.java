2726deb7e4155205644e20856b731b430c7ca725
wenbing <1447381318@qq.com>
2018-01-20 11:49:20 +0800

    222

---
+++ src/main/java/com/ezycf/managedemo/ManageDemoApplication.java
@@ -0,0 +1,12 @@
+package com.ezycf.managedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageDemoApplication.class, args);
+	}
+}
