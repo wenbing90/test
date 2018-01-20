723e863d750d257c828d3282cef7b24795a65067
wenbing <1447381318@qq.com>
2018-01-20 22:29:38 +0800

    2333333

---
+++ src/main/java/com/ezycf/managedemo/HelloController.java
@@ -0,0 +1,15 @@
+package com.ezycf.managedemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+
+    @RequestMapping("/index")
+    public String index(){
+
+        return "index.jsp";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<c:set var="ctx" value="${pageContext.request.contextPath}" />
+<!doctype html>
+<html>
+
+<head></head>
+<body>
+<h1>index</h1>
+</body>
